e6f274d012af251fd189ea70633e1aa9083fbd2c
ParkYongHo1 <103355252+ParkYongHo1@users.noreply.github.com>
2023-02-21 16:09:16 +0900

    Practice Function

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log(3213123);
+const title =document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+    title.style.color="blue";
+}
+function handleMouseEnter(){
+    title.innerText="Mouse is here";
+}
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor="tomato";
+}
+function handleWindowCopy(){
+    alert("copy!")
+}
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave );
+
+window.addEventListener("resize" , handleWindowResize);
+window.addEventListener("copy" , handleWindowCopy);
