566f76fe033689133c461713526a9f03dbb773e8
ParkYongHo1 <103355252+ParkYongHo1@users.noreply.github.com>
2023-02-23 13:25:41 +0900

    Practice classList

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-const title =document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    title.style.color="blue";
+    const clickedClass="clicked";
+    if(h1.classList.contains(clickedClass)){
+        h1.className="";
+    }else{
+        h1.className = clickedClass;
+    }
 }
-function handleMouseEnter(){
-    title.innerText="Mouse is here";
-}
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone";
-}
-function handleWindowResize(){
-    document.body.style.backgroundColor="tomato";
-}
-function handleWindowCopy(){
-    alert("copy!")
-}
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave",handleMouseLeave );
-
-window.addEventListener("resize" , handleWindowResize);
-window.addEventListener("copy" , handleWindowCopy);
+h1.addEventListener("click", handleTitleClick);
